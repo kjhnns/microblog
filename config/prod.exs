e27bd1d66b316226f3b06827ea183687f76366b5
Johannes Klumpe <klumpe@live.de>
2017-06-12 16:24:14 +0200

    fix for prods env

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :microblog, Microblog.Endpoint,
   cache_static_manifest: "priv/static/manifest.json"
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :microblog, HelloPhoenix.Repo,
+config :microblog, Microblog.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
