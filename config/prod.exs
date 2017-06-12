239b00b619497cb170c3d1f1919c2783318586c2
Johannes Klumpe <klumpe@live.de>
2017-06-12 16:58:06 +0200

    removed cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :microblog, Microblog.Endpoint,
     http: [port: {:system, "PORT"}],
     url: [scheme: "https", host: "murmuring-ravine-13598.herokuapp.com", port: 443],
     force_ssl: [rewrite_on: [:x_forwarded_proto]],
-    cache_static_manifest: "priv/static/manifest.json",
+    # cache_static_manifest: "priv/static/manifest.json",
     secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :microblog, Microblog.Repo,
